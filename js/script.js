966797a4c71b65320cfff087f2a87ea7cafcb7e7
わのすけ <wanosuke.t@gmail.com>
2025-02-01 16:34:38 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// id名で取得
+// console.log(document.querySelector("#js-title").outerHTML);
+// console.log(document.getElementById("js-title").outerHTML);
+
+// class名で取得
+// console.log(document.querySelector(".text").outerHTML);
+// const elements = document.querySelectorAll(".text");
+// elements.forEach(element => {
+//   console.log(element.outerHTML);
+// });
+
+// タグ名で取得
+// console.log(document.querySelector("h1").outerHTML);
+
+// 属性で取得
+// console.log(document.querySelector('[href="https://www.daily-trial.com/"]').outerHTML);
+
+// 取得した要素を操作
+// document.querySelector("#js-title").innerText = "Daily Trial";
+
+// HTML要素の追加
+// const button = document.createElement("a");
+// button.innerText = "ログイン";
+// button.setAttribute("class", "btn");
+// button.setAttribute("href", "https://tokyofreelance.jp/");
+// document.querySelector("#js-btn-wrap").appendChild(button);
+
+// ボタンの操作
+// document.querySelector("#js-register").click();
+
+// イベント監視
+// window.onload = function () {
+//   alert("読み込みが完了しました");
+// }
+// document.querySelector("#js-btn").addEventListener("click", function () {
+//   alert("ボタンがクリックされました");
+// });
