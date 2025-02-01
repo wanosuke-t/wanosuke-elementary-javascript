add4fed23264b2a6fcaabb3f572dc5d9a772517b
和ノ助（わのすけ） <wanosuke.t@gmail.com>
2025-02-02 08:36:27 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -57,3 +57,53 @@
 
 // 子要素のPタグ取得
 // jQuery("#js-section").children("p").css("color", "red");
+
+// クラスの追加
+// jQuery("#js-btn").addClass("btn-register");
+
+// クラスの削除
+// jQuery("#js-register").removeClass("btn-register");
+
+// CSSプロパティの設定
+// jQuery("#js-title").css("color", "red");
+// jQuery("#js-title").css({
+//   color: "red",
+//   fontSize: "80px",
+// });
+
+// 文字列の変更
+// jQuery("#js-title").text("Daily Trial");
+// const title = jQuery("#js-title").text();
+// console.log(title);
+
+// メソッドチェーン
+// jQuery("#js-title").text("Daily Trial");
+// jQuery("#js-title").css("color", "red");
+// jQuery("#js-title").text("Daily Trial").css("color", "red");
+
+// .fadeIn() フワッと表示
+// jQuery("#js-title").fadeIn(1500);
+
+// .fadeOut() フワッと消える
+// jQuery("#js-btn").fadeOut();
+
+// .slideDown() スライドして表示
+// jQuery("#js-title").slideDown();
+
+// .slideUp() スライドして消える
+// jQuery("#js-btn").slideUp();
+
+// イベントの構文
+// jQuery("セレクタ").on("イベント名", function () {});
+jQuery("#js-btn").on("click", function () {
+  jQuery("#js-title").text("Daily Trial");
+});
+
+jQuery("#js-title").on({
+  mouseenter: function () {
+    jQuery(this).css("color", "orange");
+  },
+  mouseleave: function () {
+    jQuery(this).css("color", "#082b48");
+  },
+});
